88697e2be800ce12c21c0afa4865ca00672b2c83
Karina Ionkina <kionkina@stuy.edu>
2017-12-12 19:17:26 -0500

    distance fxn complete

--- find.js
+++ find.js
@@ -13,21 +13,28 @@ console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
+    // console.log(x1);
+    // console.log(y1);
     deltaX = x0 - x1;
     deltaY = y0 - y1;
-    return Math.sqrt(deltaX * deltaX + deltaY + deltaY);
+    var ret = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
+    console.log("DISTANCE:" );
+    console.log(ret);
+    return ret;
 };
 
 
 var findIt = function(e) {
-    distToTarget = distance(targetX, targetY, e.X, e.Y);
+    distToTarget = distance(targetX, targetY, e.clientX, e.clientY);
     console.log(distToTarget);
 };
+
+
 
 /*
 your OTHER FXNS
 
 */
 
-box.addEventListener("mousemove", findIt);
+box.addEventListener("click", findIt);
 
