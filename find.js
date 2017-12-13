e16311cd4f50b53efd9a0249bfaa3d7934474d70
Karina Ionkina <kionkina@stuy.edu>
2017-12-12 19:55:52 -0500

    additions

--- find.js
+++ find.js
@@ -8,33 +8,42 @@ var targetX = boxWidth / 2;
 var targetY = boxHeight / 2;
 
 
-console.log( "box height: " + boxHeight );
-console.log( "box width: " + boxWidth );
+//console.log( "box height: " + boxHeight );
+//console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-    // console.log(x1);
-    // console.log(y1);
+    console.log(x1);
+    console.log(y1);
     deltaX = x0 - x1;
     deltaY = y0 - y1;
     var ret = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
-    console.log("DISTANCE:" );
-    console.log(ret);
     return ret;
 };
 
 
-var findIt = function(e) {
-    distToTarget = distance(targetX, targetY, e.clientX, e.clientY);
-    console.log(distToTarget);
-};
 
 
+var colors = function(e) {
+    
+    var dist = distance(targetX, targetY, e.clientX, e.clientY);
+    //    console.log("GONNA CHANGE COLA");
+    var body = document.getElementsByTagName("body");
+    maxDist =  distance(targetX, boxHeight, 0, e.clientY);
+    
+    //dist         x  
+   //--------  = -------        x = (100*dist) / maxdist
+    //maxdist     100               
+    
 
-/*
-your OTHER FXNS
+    colorVal = 225 - Math.round((225*dist) / maxDist);
+    console.log(colorVal);
+    //bad:
+    //box.setAttribute("style", "background-color: rgb(" + String(Math.round(dist)) + ", 104, 54)");
+    //good:
+    box.style.backgroundColor= "rgb(" + String(colorVal) + ", 225, 225)";
+}
 
-*/
 
-box.addEventListener("click", findIt);
+box.addEventListener("mousemove", colors);
 
